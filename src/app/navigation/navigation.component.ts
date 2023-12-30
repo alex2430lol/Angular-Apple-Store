4951e2ecae64deae890f99d021f8f4990ba0b6a6
alex2430lol <haolin2430@gmail.com>
2023-12-29 18:10:52 -0600

    Added Shopping Bag

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { state, trigger, style, animate, transition } from '@angular/animations';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrl: './navigation.component.css',
+  animations: [
+    trigger('slideDown', [
+      state('void', style({ height: '0', opacity: '0' })),
+      state('*', style({ height: '*', opacity: '1' })),
+      transition('void => *', animate('300ms ease-out')),
+      transition('* => void', animate('300ms ease-in'))
+    ])
+  ]
 })
 export class NavigationComponent {
   naviItems: string[] = [
@@ -19,4 +28,37 @@ export class NavigationComponent {
     'Accessories',
     'Support'
   ]
+  showShoppingBag: boolean = false
+
+  @ViewChild('shoppingBagDropdown') shoppingBagDropdown!: ElementRef;
+  @ViewChild('naviBar') naviBar!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+
+    // Check if the click is outside the dropdown or the navigation bar
+    if (!this.isDescendant(this.shoppingBagDropdown.nativeElement, clickedElement) &&
+      !this.isDescendant(this.naviBar.nativeElement, clickedElement)) {
+      this.showShoppingBag = false;
+    }
+  }
+
+  // Helper function to check if an element is a descendant of another element
+  private isDescendant(parent: HTMLElement, child: HTMLElement): boolean {
+    let node = child.parentNode;
+
+    while (node != null) {
+      if (node === parent) {
+        return true;
+      }
+      node = node.parentNode;
+    }
+
+    return false;
+  }
+
+  toggleShoppingBag() {
+    this.showShoppingBag = !this.showShoppingBag
+  }
 }
